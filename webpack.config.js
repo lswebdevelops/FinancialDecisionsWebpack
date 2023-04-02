fc5026dc51bb870c3c86144b4680f41733b369f0
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-04-01 23:54:59 -0300

    adding files to test on the web

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
 const path = require('path')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-
-module.exports  = {
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+module.exports = {
     mode: 'development',
     entry: {
         bundle: path.resolve(__dirname, 'src/index.js')
-    },
+},
     output:{
         path: path.resolve(__dirname, 'dist'),
         filename: '[name][contenthash].js',
         clean: true,
+        assetModuleFilename: '[name][ext]',
+
     },
+    devtool: 'source-map',
     devServer:{
         static:{
-            directory: path.resolve(__dirname, 'dist')
+            directory: path.resolve(__dirname, 'dist'),
         },
         port: 3000,
         open: true,
@@ -22,20 +25,26 @@ module.exports  = {
         historyApiFallback: true,
     },
     module:{
-        rules: [
+        rules:[
             {
-                test:/\.scss$/,
-                use:[
-                    'style-loader', 'css-loader', 'sass-loader']
-                    ,
-            },
-        ],
+            test: /\.scss$/,
+            use: ['style-loader' , 'css-loader', 'sass-loader']
+            ,
+        },
+        {
+            test:/\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource'
+        }
+    ],
+
     },
-    plugins: [
-        new HtmlWebPackPlugin({
+    plugins:[
+        new HtmlWebpackPlugin({
             title: 'Webpack App',
             filename: 'index.html',
             template: 'src/template.html',
+
         }),
-    ]
+        new BundleAnalyzerPlugin()
+    ],
 }
